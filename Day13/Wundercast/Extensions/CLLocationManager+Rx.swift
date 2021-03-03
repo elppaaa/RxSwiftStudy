c395f9f1619462cd5250eec0bb12f4c672b9e177
elppaaa <withbcd@gmail.com>
2021-03-03 22:59:33 +0900

    Day 13 - Error handling practice

---
+++ Day13/Wundercast/Extensions/CLLocationManager+Rx.swift
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2014-2016 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import Foundation
+import CoreLocation
+import RxSwift
+import RxCocoa
+
+class RxCLLocationManagerDelegateProxy : DelegateProxy<CLLocationManager, CLLocationManagerDelegate>, CLLocationManagerDelegate, DelegateProxyType {
+  static func registerKnownImplementations() {
+    self.register { manager -> RxCLLocationManagerDelegateProxy  in
+      RxCLLocationManagerDelegateProxy(parentObject: manager, delegateProxy: self)
+    }
+    
+  }
+  
+  static func currentDelegate(for object: CLLocationManager) -> CLLocationManagerDelegate? {
+    object.delegate
+  }
+  
+  static func setCurrentDelegate(_ delegate: CLLocationManagerDelegate?, to object: CLLocationManager) {
+    object.delegate = delegate
+  }
+  
+  class func currentDelegateFor(_ object: AnyObject) -> AnyObject? {
+    let locationManager: CLLocationManager = object as! CLLocationManager
+    return locationManager.delegate
+  }
+
+  class func setCurrentDelegate(_ delegate: AnyObject?, toObject object: AnyObject) {
+    let locationManager: CLLocationManager = object as! CLLocationManager
+    locationManager.delegate = delegate as? CLLocationManagerDelegate
+  }
+}
+
+extension Reactive where Base: CLLocationManager {
+
+  var delegate: DelegateProxy<CLLocationManager, CLLocationManagerDelegate> {
+//    return RxCLLocationManagerDelegateProxy.proxyForObject(base)
+    RxCLLocationManagerDelegateProxy.proxy(for: base)
+  }
+
+  var didUpdateLocations: Observable<[CLLocation]> {
+    return delegate.methodInvoked(#selector(CLLocationManagerDelegate.locationManager(_:didUpdateLocations:)))
+      .map { a in
+        return a[1] as! [CLLocation]
+    }
+  }
+
+}
